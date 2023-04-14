4edf084e0e58ecc1cebd6298510ed9f6fd2d7ba8
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2023-04-14 16:49:14 +0200

    Listening moved to logger

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,13 +1,13 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(AppModule.port);
-  return AppModule.port;
+  await app.listen(AppModule.port, () => {
+    Logger.log('App listening on ' + AppModule.port);
+  });
 }
 
-bootstrap().then((port) => {
-  console.info('App listening on ' + port);
-});
+bootstrap();
